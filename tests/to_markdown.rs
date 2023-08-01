0396c81d439384bf9db6b51db20d0a7630f43180
paletteOvO <paletteOvO@outlook.com>
2023-08-01 22:23:59 +0800

    Rewrite test_to_markdown

--- tests/to_markdown.rs
+++ tests/to_markdown.rs
@@ -7,8 +7,7 @@ mod tests {
 
    #[test]
    pub fn test_to_markdown() {
-      // https://raw.githubusercontent.com/mxstbr/markdown-test-file/master/TEST.md
-      let markdown = "# Header 1
+      let markdown = r"# Header 1
 
 ## Header 2
 
@@ -18,38 +17,66 @@ paragraph 2
 
 ## Header 3
 
-- (ledger:: opening balance)
-   - assets:bank:checking $1000
-   - assets:bank:savings $2000
-   - assets:cash $100
-   - liabilities:credit_card -$50
+* (ledger:: opening balance)
+  * assets:bank:checking $1000
+  * assets:bank:savings $2000
+  * assets:cash $100
+  * liabilities:credit\_card -$50
 
 ## Header 4
 
 ## Header 5
 
-- [ ] event (todo:: @cron(0 0 * 1 1))
+* event (todo:: @cron(0 0 \* 1 1))
 
-- (todo:: @date(2022-09-13))
-   - [ ] 13:00-15:00 aaa
-   - [ ] 18:00 ccc
-   - [ ] 15:00-18:00 bbb
-      - [ ] ddd
+* (todo:: @date(2022-09-13))
+  * 13:00-15:00 aaa
+  * 18:00 ccc
+  * 15:00-18:00 bbb
+    * ddd
+";
+      let expected = r"# Header 1
+
+## Header 2
+
+paragraph 1
+
+paragraph 2
+
+## Header 3
+
+* (ledger:: opening balance)
+  * assets:bank:checking $1000
+  * assets:bank:savings $2000
+  * assets:cash $100
+  * liabilities:credit\_card -$50
+
+## Header 4
+
+## Header 5
+
+* event (todo:: @cron(0 0 \* 1 1))
+
+* (todo:: @date(2022-09-13))
+  * 13:00-15:00 aaa
+  * 18:00 ccc
+  * 15:00-18:00 bbb
+    * ddd
 ";
 
       let mut options = ParseOptions::gfm();
       options.constructs = Constructs {
          // frontmatter: true,
-         ..Constructs::gfm()
+         ..Constructs::default()
       };
       let root_node = to_mdast(markdown, &options).unwrap();
-      let markdown = to_markdown(
+      let markdown_output = to_markdown(
          &root_node,
          Options {
             ..Options::default()
          },
       );
 
-      println!("markdown: {}", markdown);
+      assert!(markdown_output == expected);
    }
 }
